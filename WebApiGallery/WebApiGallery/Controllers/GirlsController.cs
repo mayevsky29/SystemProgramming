151cdc96b0215d30fd4b0401a64f715b9c3f8ac0
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-05-04 17:36:49 +0300

    add webapi

---
+++ WebApiGallery/WebApiGallery/Controllers/GirlsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiGallery.Models;
+
+namespace WebApiGallery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GirlsController : ControllerBase
+    {
+
+
+        [HttpGet]
+        [Route("search")]
+        public IActionResult SearchGrils()
+        {
+            var list = new List<GirlVM>()
+            {
+                new GirlVM
+                {
+                    Name = "Саша Маргалова",
+                    Age=33,
+                    Height=198,
+                    Weight=95,
+                    Image="1.png"
+                },
+                new GirlVM
+                {
+                    Name = "Наташа Ростова",
+                    Age=18,
+                    Height=202,
+                    Weight=102,
+                    Image = "2.jpg"
+                },
+            };
+            return Ok(list);
+        }
+    }
+}
